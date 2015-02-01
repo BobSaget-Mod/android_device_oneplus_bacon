a55a5ad7a9ea15bff992f4e9129c203cac373f3d
Joe Maples <frap129@gmail.com>
2015-02-01 18:46:48 -0500

    Add SaberMod Variables

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -14,6 +14,12 @@
 # limitations under the License.
 #
 
+# Some SaberMod variables before common
+O3_OPTIMIZATIONS := true
+TARGET_SM_AND := 4.8
+TARGET_SM_KERNEL := 4.9
+TARGET_ARCH := arm
+
 # Inherit from MSM8974 common
 -include device/oppo/msm8974-common/BoardConfigCommon.mk
 
